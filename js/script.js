cdedfbc97db149775f9cb22d83dbe58cc46ae692
David Hatch <hatchdavidv@gmail.com>
2022-11-03 17:15:09 -0600

    querySelector items

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const endtime = document.querySelector('#Time-left');
+const resultScore = document.querySelector('#Result')
+const StartPauseButton = document.querySelector('#Start-Pause-button')
+const matrixSquare = document.querySelectorAll('.Matrix div')
+
