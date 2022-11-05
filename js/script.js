2cd55d65c56fdbd3a25b7c22201bb2fd6b4993f0
David Hatch <hatchdavidv@gmail.com>
2022-11-05 11:40:14 -0600

    movements

--- js/script.js
+++ js/script.js
@@ -3,3 +3,32 @@ const resultScore = document.querySelector('#Result')
 const StartPauseButton = document.querySelector('#Start-Pause-button')
 const matrixSquare = document.querySelectorAll('.Matrix div')
 
+let actualIndex = 76;
+let rowLength = 9;
+function frogMovement(tecla) {
+    //console.log('Movimiento');
+    matrixSquare[actualIndex].classList.remove('frog')
+    switch(tecla.keyCode){
+        case 37:
+            console.log('left move');
+            actualIndex -=1; 
+            break
+        case 39:
+            console.log('right move');
+            actualIndex +=1;
+            break
+        case 38:
+            console.log('up move');
+            actualIndex -= rowLength
+            break
+        case 40:
+            console.log('down move');
+            actualIndex += rowLength
+            break;
+    }
+
+
+    matrixSquare[actualIndex].classList.add('frog')
+}
+
+document.addEventListener('keydown', frogMovement)
