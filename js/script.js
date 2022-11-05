b51f58f4062df838d1bb14f17ed826eec4fe6c07
David Hatch <hatchdavidv@gmail.com>
2022-11-05 13:44:52 -0600

    moves trees left

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const endtime = document.querySelector('#Time-left');
 const resultScore = document.querySelector('#Result')
 const StartPauseButton = document.querySelector('#Start-Pause-button')
 const matrixSquare = document.querySelectorAll('.Matrix div')
+const treesLeft = document.querySelectorAll('.Tree-left')
+
 
 let actualIndex = 76;
 let rowLength = 9;
@@ -28,3 +30,37 @@ function frogMovement(tecla) {
 }
 
 document.addEventListener('keydown', frogMovement)
+
+function moveTrees(){
+    treesLeft .forEach(treeLeft => treeMovementLeft(treeLeft))
+}
+
+moveTrees
+
+function treeMovementLeft(treeLeft){
+    switch(true){
+        case treeLeft.classList.contains('l1'):
+            treeLeft.classList.remove('l1')
+            treeLeft.classList.add('l2')
+            break
+        case treeLeft.classList.contains('l2'):
+            treeLeft.classList.remove('l2')
+            treeLeft.classList.add('l3')
+            break
+        case treeLeft.classList.contains('l3'):
+            treeLeft.classList.remove('l3')
+            treeLeft.classList.add('l4')
+            break
+        case treeLeft.classList.contains('l4'):
+            treeLeft.classList.remove('l4')
+            treeLeft.classList.add('l5')
+            break
+        case treeLeft.classList.contains('l5'):
+            treeLeft.classList.remove('l5')
+            treeLeft.classList.add('l1')
+            break
+    }
+}
+
+
+setInterval(moveTrees, 1000)
